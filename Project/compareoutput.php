70113c501f7a6066c5d0f21f292b955c3bc68337
System Administrator <root@Pranavs-MacBook-Pro.local>
2015-04-13 16:13:11 -0400

    new files

---
+++ Project/compareoutput.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Page Title -->
+    <title>Compare Output</title>
+
+    <style>
+    #container{
+        width:100%; 
+        height:400px;
+        padding: 50px;
+    }
+    </style>
+</head>
+
+<body>
+    My first php program.
+    <?php echo $_POST["startyear"] ?>
+</body>
